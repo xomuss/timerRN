2afb1a0886019a8155a8a0d266120bbb18f2447e
xomuss <olexandr.khomenko88@gmail.com>
2021-12-12 23:36:25 +0200

    timer refactor

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -14,8 +14,9 @@ const Timer = () => {
   //***handlers***
 
   const onStartButtonClick = () => {
-    run();
-    setIntervalId(setInterval(run, 10));
+    // runTimer();
+    setIntervalId(setInterval(runTimer, 10));
+    console.log('log--------', intervalId);
   };
 
   const onButtonStopClick = () => {
@@ -26,7 +27,7 @@ const Timer = () => {
     setTime({ms: 0, s: 0, m: 0, h: 0});
   };
 
-  const run = () => {
+  const runTimer = () => {
     if (updatedM === 60) {
       updatedH++;
       updatedM = 0;
@@ -45,49 +46,46 @@ const Timer = () => {
 
   //***renders methods***
 
-  const TimerNumbers = () => {
+  const renderTimerNumbers = () => {
+    const hours = time.h >= 10 ? time.h : '0' + time.h;
+    const minutes = time.m >= 10 ? time.m : '0' + time.m;
+    const seconds = time.s >= 10 ? time.s : '0' + time.s;
+    const miliSeconds = time.ms >= 10 ? time.ms : '0' + time.ms;
+
     return (
       <View style={styles.timerContainer}>
-        <Text style={styles.timerNumbers}>
-          {time.h >= 10 ? time.h : '0' + time.h}
-        </Text>
-        <Text style={styles.timerNumbers}>
-          {time.m >= 10 ? time.m : '0' + time.m}
-        </Text>
-        <Text style={styles.timerNumbers}>
-          {time.s >= 10 ? time.s : '0' + time.s}
-        </Text>
-        <Text style={styles.timerNumbers}>
-          {time.ms >= 10 ? time.ms : '0' + time.ms}
-        </Text>
+        <Text style={styles.timerNumbers}>{hours}</Text>
+        <Text style={styles.timerNumbers}>{minutes}</Text>
+        <Text style={styles.timerNumbers}>{seconds}</Text>
+        <Text style={styles.timerNumbers}>{miliSeconds}</Text>
       </View>
     );
   };
 
-  const TimerControls = () => {};
-
-  //***
+  const renderTimerControls = () => (
+    <View style={styles.timerControlsContainer}>
+      <TouchableOpacity
+        style={styles.timerControls}
+        onPress={onStartButtonClick}>
+        <Text>START</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.timerControls}
+        onPress={onButtonStopClick}>
+        <Text>STOP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.timerControls}
+        onPress={onButtonClearClick}>
+        <Text>CLEAR</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <TimerNumbers />
-      <View style={styles.timerControlsContainer}>
-        <TouchableOpacity
-          style={styles.timerControls}
-          onPress={onStartButtonClick}>
-          <Text>START</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.timerControls}
-          onPress={onButtonStopClick}>
-          <Text>STOP</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.timerControls}
-          onPress={onButtonClearClick}>
-          <Text>CLEAR</Text>
-        </TouchableOpacity>
-      </View>
+      {renderTimerNumbers()}
+      {renderTimerControls()}
     </View>
   );
 };
